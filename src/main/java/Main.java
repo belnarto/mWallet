8574192ecc6b2aee44421909b457543dffafba18
Anton Zinchenko <belnarto@gmail.com>
2019-06-03 19:06:35 +0300

    WAR export configuration

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,5 @@
-import models.Wallet;
-import services.CurrencyService;
-import services.WalletService;
-
-import java.sql.SQLException;
-
 public class Main {
-
-
     public static void main(String[] args) {
-        System.out.println(CurrencyService.findByName("USD"));
+
     }
 }
