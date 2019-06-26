2953e240ce8f7eb504c458fc489a05cc35ad3b2e
Anton Zinchenko <belnarto@gmail.com>
2019-06-26 17:53:43 +0300

    test of rollback

--- src/main/java/com/vironit/mWallet/dao/impl/UserDaoImpl.java
+++ src/main/java/com/vironit/mWallet/dao/impl/UserDaoImpl.java
@@ -15,6 +15,10 @@ public class UserDaoImpl implements UserDao {
         return HibernateSessionFactoryUtil.getSessionFactory().openSession().get(User.class, id);
     }
 
+    public User findById2(int id) {
+        return HibernateSessionFactoryUtil.getSessionFactory().openSession().get(User.class, id);
+    }
+
     public User findByLogin(String login) {
         Session session = HibernateSessionFactoryUtil.getSessionFactory().openSession();
         User user = (User) session
